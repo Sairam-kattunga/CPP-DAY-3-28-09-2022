3474445d770c03934ba4121972691933f8605b7d
Sairam-kattunga <112626004+Sairam-kattunga@users.noreply.github.com>
2022-09-29 10:50:03 +0530

    Add files via upload

---
+++ day 3 (4)  maximun number to print in the array c++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+#define SIZE 50 
+
+int main()
+
+{
+    int array[SIZE];
+    int i, max, min, size;
+    cout<<"Enter size of the array: ";
+    cin>>size;
+    cout<<"\n Enter "<<size <<" elements in the array: ";
+    for(i=0; i<size; i++)
+        cin>>array[i];
+    max = array[0];
+    for(i=1; i<size; i++)
+    {
+        if(array[i] > max)
+            max = array[i];
+    }
+    cout<<"\nMaximum element =" << max << "\n";
+    return 0;
+
+}
